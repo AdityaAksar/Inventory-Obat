c420f05dbb92b0a861617975c79038a259e2a16f
AdityaAksar <54211361+AdityaAksar@users.noreply.github.com>
2025-11-24 21:37:50 +0800

    stle(ui): redesign main activity with custom search bar and chip-style tabs

--- app/src/main/java/com/example/inventoryobat/MainActivity.java
+++ app/src/main/java/com/example/inventoryobat/MainActivity.java
@@ -2,11 +2,12 @@ package com.example.inventoryobat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.inventoryobat.databinding.ActivityMainBinding;
@@ -23,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
         viewModel = new MainViewModel();
 
         if (!PermissionHelper.checkPermission(this)) {
@@ -30,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setupViewPager();
+        setupSearchListener();
 
         viewModel.getErrorMessage().observe(this, errorMsg -> {
             if (errorMsg != null) {
@@ -41,12 +47,21 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, TambahObatActivity.class);
             startActivity(intent);
         });
-        setupViewPager();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void setupSearchListener() {
+        binding.edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                filterCurrentFragment(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 
     private void setupViewPager() {
@@ -63,50 +78,20 @@ public class MainActivity extends AppCompatActivity {
 
         binding.viewPager.setAdapter(adapter);
         binding.tabLayout.setupWithViewPager(binding.viewPager);
-    }
 
-
-//    private void setupViewPager() {
-//        adapter = new ViewPagerAdapter(getSupportFragmentManager());
-//
-//        adapter.addFragment(ObatFragment.newInstance("SEMUA"), "Semua");
-//
-//        for (JenisObat jenis : JenisObat.values()) {
-//            adapter.addFragment(
-//                    ObatFragment.newInstance(jenis.name()),
-//                    jenis.getDisplayName()
-//            );
-//        }
-//
-//        binding.viewPager.setAdapter(adapter);
-//        binding.tabLayout.setupWithViewPager(binding.viewPager);
-//    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem item = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) item.getActionView();
-        searchView.setQueryHint("Cari nama obat...");
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                filterCurrentFragment(query);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                filterCurrentFragment(newText);
-                return true;
-            }
-        });
-        return true;
+        for (int i = 0; i < binding.tabLayout.getTabCount(); i++) {
+            View tab = ((ViewGroup) binding.tabLayout.getChildAt(0)).getChildAt(i);
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) tab.getLayoutParams();
+            p.setMargins(0, 0, 16, 0);
+            tab.requestLayout();
+        }
     }
 
     private void filterCurrentFragment(String keyword) {
         int pos = binding.viewPager.getCurrentItem();
         ObatFragment fragment = (ObatFragment) adapter.getItem(pos);
-        fragment.filterData(keyword);
+        if (fragment != null) {
+            fragment.filterData(keyword);
+        }
     }
 }
